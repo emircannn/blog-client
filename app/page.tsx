ef82fb480e0798a2161744910cbbd00fa051dd19
Emircan  Yaşar <yasar.emircann@gmail.com>
2023-09-13 21:04:58 +0300

    remove delay

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import ClientOnly from "@/components/layouts/ClientOnly";
 import HomePage from "@/components/pages/Home/HomePage";
 
-function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
 export default async function Home() {
-  await delay(6000)
   return (
     <ClientOnly>
       <HomePage/>
