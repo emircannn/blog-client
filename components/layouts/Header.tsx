36741a39e280435eaef87cb0bc71d4b8c0ea6478
Emircan  Yaşar <yasar.emircann@gmail.com>
2023-09-22 00:59:19 +0300

    header fix

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -26,13 +26,17 @@ const Header = () => {
     return ( 
     <header className=" bg-white dark:bg-darkerColor sticky top-0 border-b dark:border-darkColor border-lightColor z-50">
         <div className="flex items-center justify-between gap-3 container min-h-full">
+            <div className="w-1/2 md:w-[20%] flex">
             <Logo/>
+            </div>
 
+            <div className="w-[60%] flex items-center justify-center">
             <Navbar 
             items={navbar}
             />
+            </div>
 
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3 w-1/2 md:w-[20%] justify-end">
                 <SearchModal/>
                 <div className="max-md:hidden">
                 <ModeToggle/>
